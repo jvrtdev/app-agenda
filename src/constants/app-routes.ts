1677546e2f9e1d0305e02bf91b53a85498fc72b8
jvrtdevmac <joaovitorromanoteixeira@gmail.com>
2024-05-06 01:25:47 -0300

    :sparkles: feat: const with app_routes

---
+++ src/constants/app-routes.ts
@@ -0,0 +1,13 @@
+export const APP_ROUTES = {
+  private: {
+    agendify: {
+      name: "/agendify"
+    }
+  },
+  public: {
+    home: '/',
+    cadastro: '/sign-up',
+    login: '/sign-in'
+  }
+
+}
