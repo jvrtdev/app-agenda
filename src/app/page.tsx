d902cc962f4f8a56d508bde9ea42fc309961fe6c
jvrtdevmac <joaovitorromanoteixeira@gmail.com>
2024-05-06 01:31:47 -0300

    :recycle: refactor: add photos and Wrapper responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,28 @@ import Image from "next/image";
 import Link from "next/link";
 import foto1 from "@/../public/foto1.svg"
 import foto2 from "@/../public/foto2.svg"
+import foto3 from "@/../public/foto3.svg"
+import { CardsMap } from "@/components/Card";
+import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
+
 
 
 export default function Home() {
   return (
     <main className="flex justify-center flex-col items-center gap-2 ">
-      <div className="max-w-screen-xl border-2 border-red-600 w-full">
-        <h1 className="text-4xl font-bold text-center mt-10">Aumente a Eficiência do Seu Negócio com JAMF!</h1>
+      <MaxWidthWrapper>
+        <h1 className="text-4xl font-bold text-center mt-10">Aumente a Eficiência do Seu Negócio com a Agendify!</h1>
         
-        <div className="w-full mt-10 mb-28 flex items-center justify-evenly">
+        <div className="w-full h-screen flex flex-wrap items-center justify-center">
           <div>
             <h1 className="text-4xl font-bold mb-2">Bem vindo!</h1>
-            <p className=" max-w-md mb-5">Transforme a maneira como você gerencia os agendamentos dos seus serviços com [Nome do SaaS]. Nossa plataforma simplifica o processo de agendamento, permitindo que sua empresa se concentre no que faz de melhor: proporcionar experiências excepcionais aos seus clientes.</p>
+            <p className=" max-w-md mb-5">Transforme a maneira como você gerencia os agendamentos dos seus serviços com Agendify. Nossa plataforma simplifica o processo de agendamento, permitindo que sua empresa se concentre no que faz de melhor: proporcionar experiências excepcionais aos seus clientes.</p>
             <Link href="/sign-up" className={cn("border-2 p-2 border-black rounded-full max-w-max hover:scale-150 cursor-pointer ")}>Começar</Link>
           </div>
           <Image src={foto1} alt="foto time empresa"/>
         </div>
         
-        <div className="w-full mt-10 mb-20 flex  items-center justify-evenly">
+        <div className="w-full h-screen flex flex-wrap  items-center justify-center">
           <Image src={foto2} alt="foto barbeiro cliente empresa"/>
           <div>
             <h1 className="text-4xl font-bold mb-2 max-w-md">Melhore a Experiência dos Seus Clientes</h1>
@@ -28,12 +32,14 @@ export default function Home() {
           </div>
         </div>
 
-
-
-
-
-
-      </div>  
+        <div className="w-full h-screen flex flex-wrap items-center justify-center">
+          <div className="">
+            <h1 className="text-4xl font-bold mb-2 max-w-md">Por Que Escolher a Agendify?</h1>
+            <CardsMap />
+          </div>
+          <Image src={foto3} alt="foto barbeiro cliente empresa"/>
+        </div>
+      </MaxWidthWrapper>
     </main>
   );
 }
